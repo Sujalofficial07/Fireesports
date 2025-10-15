b954b4e386caab628e78836d63891409f99f167f
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-15 20:10:42 +0530

    Update SupabaseClient.kt

--- app/src/main/java/com/fireesports/data/remote/SupabaseClient.kt
+++ app/src/main/java/com/fireesports/data/remote/SupabaseClient.kt
@@ -2,7 +2,7 @@ package com.fireesports.data.remote
 
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.gotrue.Auth
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.realtime.Realtime
 import io.github.jan.supabase.storage.Storage
@@ -16,7 +16,7 @@ class SupabaseClientProvider @Inject constructor() {
         createSupabaseClient(
             supabaseUrl = "https://datrazwjswjmvpephmzo.supabase.co",
             supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhdHJhendqc3dqbXZwZXBobXpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzODQ5NzcsImV4cCI6MjA3NTk2MDk3N30.eOWIovkbmMX71-mXPi3OfKralTKCgRv4WqX4bao6j3I"
-        ) {
+          ) {
             install(Auth)
             install(Postgrest)
             install(Realtime)
