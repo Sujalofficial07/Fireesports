272843cad771029866092b32498c2c4abc13de2d
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-15 20:41:02 +0530

    Create Color.kt

---
+++ app/src/main/java/com/fireesports/ui/Color.kt
@@ -0,0 +1,26 @@
+package com.fireesports.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary colors
+val NeonAqua = Color(0xFF00FFFF)  // From colors.xml
+val NeonPink = Color(0xFFFF006E)   // From colors.xml
+val NeonPurple = Color(0xFFBB00FF) // From colors.xml
+
+// Background colors
+val DarkBackground = Color(0xFF0A0A0F) // From colors.xml
+val DarkSurface = Color(0xFF15151F)    // From colors.xml
+val DarkCard = Color(0xFF1E1E2E)       // From colors.xml
+
+// Text colors
+val TextPrimary = Color(0xFFFFFFFF)     // From colors.xml
+val TextSecondary = Color(0xFFB3FFFFFF)  // From colors.xml
+val TextTertiary = Color(0xFF808080)     // Adding missing color
+
+// Status colors
+val Success = Color(0xFF00E676)
+val Warning = Color(0xFFFFB74D)
+val Error = Color(0xFFFF5252)
+
+// Additional theme colors
+val DarkBorder = Color(0xFF2A2A3C)
