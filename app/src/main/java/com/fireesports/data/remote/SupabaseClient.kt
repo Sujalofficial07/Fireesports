716cb802c5ae16f67a3c2bbe07fa5103cbf7b310
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-15 17:18:26 +0530

    Update SupabaseClient.kt

--- app/src/main/java/com/fireesports/data/remote/SupabaseClient.kt
+++ app/src/main/java/com/fireesports/data/remote/SupabaseClient.kt
@@ -14,8 +14,8 @@ class SupabaseClientProvider @Inject constructor() {
     
     val client: SupabaseClient by lazy {
         createSupabaseClient(
-            supabaseUrl = "YOUR_SUPABASE_URL",
-            supabaseKey = "YOUR_SUPABASE_ANON_KEY"
+            supabaseUrl = "https://datrazwjswjmvpephmzo.supabase.co",
+            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhdHJhendqc3dqbXZwZXBobXpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzODQ5NzcsImV4cCI6MjA3NTk2MDk3N30.eOWIovkbmMX71-mXPi3OfKralTKCgRv4WqX4bao6j3I"
         ) {
             install(Auth)
             install(Postgrest)
